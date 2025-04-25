2243dfe206cc04ccafbcb1c7fdada110834a4ca5
Rals <raulgallardo6@gmail.com>
2025-04-25 11:15:49 -0600

    ✨Add exercise 1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -13,17 +13,28 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
   const randomAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length)
     setSelected(randomIndex)
   }
 
+  const voteAnecdote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
       <button onClick={randomAnecdote}>
         Next anecdote
+      </button>
+      <button onClick={voteAnecdote}>
+        Vote
       </button>
     </div>
   )
